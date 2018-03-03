273682a6cdbc236e5b3af74c4e0e55ff5c999fb3
vicmoh <vimoh1213@gmail.com>
2018-03-03 16:06:01 -0500

    main

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/***********************************
+ * Vicky Mohammad
+ * 0895381
+ ***********************************/
+
+//imort libs
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//main func to run
+int main(int argc, char** argv){
+    
+}//end main
